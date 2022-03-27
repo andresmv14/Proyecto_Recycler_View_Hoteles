4a223d58be0f892a99a0219a6aef22d8433ca3f9
Andres <andres.mv14@outlook.com>
2022-03-27 13:30:42 -0600

    Comentario

--- app/src/main/java/com/mendozavalerio/hoteles/adaptador/AdaptadorHotel.kt
+++ app/src/main/java/com/mendozavalerio/hoteles/adaptador/AdaptadorHotel.kt
@@ -1,7 +1,6 @@
 package com.mendozavalerio.hoteles.adaptador
 
 import android.view.LayoutInflater
-import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
